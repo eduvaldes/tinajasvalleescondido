9af87f628796a85df43f9b2e6e342a31729436f8
Eduardo Valdés Escalona <54859059+eduvaldes@users.noreply.github.com>
2023-08-23 20:56:13 -0400

    Create index.php
    inicio

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tinajas Valle Escondido</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+        header {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1rem;
+        }
+        .container {
+            max-width: 1200px;
+            margin: 0 auto;
+            padding: 2rem;
+        }
+        .intro {
+            text-align: center;
+            margin-bottom: 2rem;
+        }
+        .albums {
+            text-align: center;
+            margin-top: 2rem;
+        }
+        .album {
+            margin-bottom: 2rem;
+        }
+        .album h3 {
+            font-size: 1.2rem;
+            margin-bottom: 1rem;
+        }
+        .album-images img {
+            max-width: 100%;
+            height: auto;
+            margin: 0.5rem;
+            border: 1px solid #ddd;
+            border-radius: 5px;
+        }
+        .map-container {
+            max-width: 100%;
+            overflow: hidden;
+            position: relative;
+            height: 0;
+            padding-bottom: 56.25%; /* Proporción de aspecto 16:9 */
+        }
+        .map-container iframe {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        }
+        footer {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1rem;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Tinajas Valle Escondido</h1>
+        <p>Arriendo y Venta de Tinajas 100% Madera de Ciprés con Calderas Galvanizadas en Caliente</p>
+    </header>
+    <div class="container">
+        <div class="intro">
+            <h2>Bienvenidos a Tinajas Valle Escondido</h2>
+            <p>Somos un emprendimiento familiar dedicado a brindar tinajas de madera de ciprés de alta calidad, perfectas para disfrutar de momentos especiales.</p>
+        </div>
+        <div class="albums">
+            <h2>Galería de Imágenes</h2>
+            <div class="album">
+                <h3>Álbum 1 - Tinajas de Madera</h3>
+                <div class="album-images">
+                    <img src="ruta-a-imagen-1.jpg" alt="Imagen 1">
+                    <img src="ruta-a-imagen-2.jpg" alt="Imagen 2">
+                    <!-- Agrega más imágenes aquí -->
+                </div>
+            </div>
+            <div class="album">
+                <h3>Álbum 2 - Eventos Especiales</h3>
+                <div class="album-images">
+                    <img src="ruta-a-imagen-3.jpg" alt="Imagen 3">
+                    <img src="ruta-a-imagen-4.jpg" alt="Imagen 4">
+                    <!-- Agrega más imágenes aquí -->
+                </div>
+            </div>
+        </div>
+        <div class="map-container">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1dXXXXXXXXXXXXXXXXXXXX!2d-72.3173315!3d-36.4655089!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3cebc2936867ce5a!2sTinajas%20Valle%20Escondido!5e0!3m2!1sen!2sus!4v1630052131681!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+    </div>
+    <footer>
+        <p>&copy; 2023 Tinajas Valle Escondido - Todos los derechos reservados</p>
+    </footer>
+</body>
+</html>
